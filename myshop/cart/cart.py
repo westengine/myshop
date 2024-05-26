ec9c80873313f0e47b6fe8b66ae7d5b86413947e
westengine <emiadejesunifemi@gmail.com>
2024-05-26 23:12:57 +0100

    feat: add class Cart in cart.py

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -40,3 +40,36 @@ class Cart:
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products
+        from the database.
+        """
+        product_ids = self.cart.keys()
+        #get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+    
+    def __len__(self):
+        """
+        Count all items in the cart.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+    
+    def get_total_price(self):
+        """
+        Calculate the total cost of items in the cart.
+        """
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+    
+    def clear(self):
+        #remove cart from session
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
