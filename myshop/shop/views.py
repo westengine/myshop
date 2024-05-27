60a37e80bf26ad1a2e0d34f0148fa290207bedb6
westengine <emiadejesunifemi@gmail.com>
2024-05-27 18:49:25 +0100

    feat: add to cart button in product detail page

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 # View to list all the products
@@ -22,6 +23,8 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product/detail.html',
-                  {'product': product})
+                  {'product': product,
+                  'cart_product_form': cart_product_form})
